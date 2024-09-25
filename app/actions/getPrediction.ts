bccb9670f02b0bcb0f0cbf3e7369faffae84af2c
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-24 22:00:39 -0400

    part4

--- app/actions/getPrediction.ts
+++ app/actions/getPrediction.ts
@@ -2,7 +2,10 @@
 
 import { fetchStocksData } from "./stocksActions"
 
-
+/**
+ * Get Prediction
+ * @param symbols 
+ */
 export async function getPrediction(symbols: string[]) {
     const stocksDataResults = await fetchStocksData(symbols)
     console.log(stocksDataResults);
